ab59263e938ff1b53fa1b8e7d8b767274c47b275
m_juhdi <mjfahriza@gmail.com>
2026-07-24 21:51:47 +0700

    clear banget inimah

--- resources/views/trainers/index.blade.php
+++ resources/views/trainers/index.blade.php
@@ -12,7 +12,10 @@
     </div>
 
     @if(session('success'))
-        <div class="alert alert-success">{{ session('success') }}</div>
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{ session('success') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
     @endif
 
     <div class="card shadow mb-4">
